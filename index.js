5ffea0eb811cd86783a629e3ec356bb74f893eab
Brandon Kim <bkim95ca@gmail.com>
2018-03-08 09:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name} you are number ${katzDeliLine.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  
 }
